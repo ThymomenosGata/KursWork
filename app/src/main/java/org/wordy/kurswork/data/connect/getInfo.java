8a6646eaf73d9338e42124a4b78af26aa3fb0f83
ThymomenosGata <ThymomenosGata@MacBook-Air-mac.local>
2019-03-06 16:34:09 +0300

    the end getting info from db

--- app/src/main/java/org/wordy/kurswork/data/connect/getInfo.java
+++ app/src/main/java/org/wordy/kurswork/data/connect/getInfo.java
@@ -1,6 +1,9 @@
 package org.wordy.kurswork.data.connect;
 
+import org.wordy.kurswork.data.tables.Group;
+import org.wordy.kurswork.data.tables.News;
 import org.wordy.kurswork.data.tables.Professor;
+import org.wordy.kurswork.data.tables.Students;
 import org.wordy.kurswork.data.tables.User;
 
 import java.sql.Connection;
@@ -69,6 +72,88 @@ public class getInfo {
             e.printStackTrace();
         }
         return professors;
+    }
+
+    public List<Students> selectStudent() {
+        List<Students> students = new ArrayList<>();
+        mQuery = "select * from students";
+        mConnect = new ConnectingDB();
+        mCon = mConnect.getmCon();
+        try {
+            stmt = mCon.createStatement();
+            rs = stmt.executeQuery(mQuery);
+            while (rs.next()) {
+                students.add(
+                        new Students(
+                                rs.getInt(1),
+                                rs.getString(2),
+                                rs.getString(3),
+                                rs.getString(4),
+                                rs.getString(5),
+                                rs.getInt(6),
+                                rs.getInt(7),
+                                rs.getString(8),
+                                rs.getInt(9)
+                        )
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return students;
+    }
+
+    public List<Group> selectGroup() {
+        List<Group> groups = new ArrayList<>();
+        mQuery = "select * from group";
+        mConnect = new ConnectingDB();
+        mCon = mConnect.getmCon();
+        try {
+            stmt = mCon.createStatement();
+            rs = stmt.executeQuery(mQuery);
+            while (rs.next()) {
+                groups.add(
+                        new Group(
+                                rs.getInt(1),
+                                rs.getString(2),
+                                rs.getInt(3),
+                                rs.getString(4),
+                                rs.getString(5)
+                        )
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return groups;
+    }
+
+    public List<News> selectNews() {
+        List<News> news = new ArrayList<>();
+        mQuery = "select * from news";
+        mConnect = new ConnectingDB();
+        mCon = mConnect.getmCon();
+        try {
+            stmt = mCon.createStatement();
+            rs = stmt.executeQuery(mQuery);
+            while (rs.next()) {
+                news.add(
+                        new News(
+                                rs.getInt(1),
+                                rs.getString(2),
+                                rs.getString(3),
+                                rs.getString(4),
+                                rs.getString(5),
+                                rs.getString(6),
+                                rs.getInt(7),
+                                rs.getInt(8)
+                        )
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return news;
     }
 
 }
