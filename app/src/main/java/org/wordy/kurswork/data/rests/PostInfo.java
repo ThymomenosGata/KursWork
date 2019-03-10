f6a8a2815ee2cb8b2ba8da5d3ab46cb815ac3795
ThymomenosGata <kostaki97@mail.ru>
2019-03-11 00:05:18 +0300

    start post info class

--- app/src/main/java/org/wordy/kurswork/data/rests/PostInfo.java
+++ app/src/main/java/org/wordy/kurswork/data/rests/PostInfo.java
@@ -1,4 +1,70 @@
 package org.wordy.kurswork.data.rests;
 
+import org.wordy.kurswork.data.PortalRest;
+import org.wordy.kurswork.data.tables.Group;
+
+import java.io.IOException;
+
 public class PostInfo {
+
+    //TODO: доделать епту нада
+
+    private PortalRest mPortal;
+
+    public PostInfo() {
+        this.mPortal = PortalRest.getPortal();
+    }
+
+    public boolean insertGroup(Group group) {
+        try {
+            mPortal.get("insert into group(id, name, count, faculty, date_last_modify) " +
+                    "values(nextVal('id')," + group.getName() + "," + group.getCount()+ "," + group.getFaculty() + ","
+                    + group.getDate_last_modify() + ")").body().string();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean insertNews() {
+        try {
+            mPortal.get("insert into group() values()").body().string();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean insertProfessor() {
+        try {
+            mPortal.get("insert into group() values()").body().string();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean insertStudents() {
+        try {
+            mPortal.get("insert into group() values()").body().string();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean insertUsers() {
+        try {
+            mPortal.get("insert into group() values()").body().string();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
