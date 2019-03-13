d005b154b2e2f3da0f8adc0c93de62ecde17c506
ThymomenosGata <kostaki97@mail.ru>
2019-03-13 15:12:25 +0300

    reload button

--- app/src/main/java/org/wordy/kurswork/screens/MainActivity.java
+++ app/src/main/java/org/wordy/kurswork/screens/MainActivity.java
@@ -1,9 +1,11 @@
 package org.wordy.kurswork.screens;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -14,14 +16,25 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import org.wordy.kurswork.R;
+import org.wordy.kurswork.data.DataBase;
+import org.wordy.kurswork.data.rests.GetInfo;
+import org.wordy.kurswork.data.rests.PostInfo;
+import org.wordy.kurswork.data.tables.Group;
+import org.wordy.kurswork.data.tables.News;
+import org.wordy.kurswork.data.tables.Professor;
+import org.wordy.kurswork.data.tables.Students;
+import org.wordy.kurswork.data.tables.User;
 import org.wordy.kurswork.screens.group.GroupFragment;
 import org.wordy.kurswork.screens.news.NewsFragment;
 import org.wordy.kurswork.screens.professor.ProfessorFragment;
 import org.wordy.kurswork.screens.students.StudentsFragment;
 import org.wordy.kurswork.screens.user.UserFragment;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -30,6 +43,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private static final String APP_PREFERENCES_UPD = "upd";
     private SharedPreferences mSettings;
     private Editor editor;
+    private PostInfo postInfo;
+    private GetInfo getInfo;
+    private DataBase dataBase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +62,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        postInfo = new PostInfo();
+        getInfo = new GetInfo();
+        dataBase = DataBase.getDataBase(getApplication());
 
         mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
         editor = mSettings.edit();
@@ -71,6 +91,63 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } else {
             super.onBackPressed();
         }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_reload: {
+                new AsyncTask<Void, Void, Boolean>() {
+
+                    @Override
+                    protected Boolean doInBackground(Void... voids) {
+                        dataBase.newsDao().deleteAll();
+                        dataBase.professorDao().deleteAll();
+                        dataBase.studentsDao().deleteAll();
+                        dataBase.groupsDao().deleteAll();
+                        dataBase.usersDao().deleteAll();
+                        List<Group> groups = getInfo.selectGroup();
+                        for (Group group : groups) {
+                            dataBase.groupsDao().insert(group);
+                        }
+                        List<News> news = getInfo.selectNews();
+                        for (News news1 : news) {
+                            dataBase.newsDao().insert(news1);
+                        }
+                        List<Professor> professors = getInfo.selectProfessor();
+                        for (Professor professor : professors) {
+                            dataBase.professorDao().insert(professor);
+                        }
+                        List<Students> students = getInfo.selectStudents();
+                        for (Students student : students) {
+                            dataBase.studentsDao().insert(student);
+                        }
+                        List<User> users = getInfo.selectUsers();
+                        for (User user : users) {
+                            dataBase.usersDao().insert(user);
+                        }
+                        return true;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Boolean aBoolean) {
+                        super.onPostExecute(aBoolean);
+
+                    }
+                }.execute();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.reload, menu);
+        return true;
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
